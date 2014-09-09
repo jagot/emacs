85fffb3553fa864713e3c3f2937bce1db2272f67
Stefanos Carslström <stefanos.carlstrom@gmail.com>
2014-09-09 11:03:51 +0200

    Moved custom variables into separate file

---
+++ home/.emacs.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doc-view-resolution 200))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-info ((t (:height 1.5))))
+ '(org-document-title ((t (:weight bold :height 2.0))))
+ '(org-level-1 ((t (:inherit outline-1 :weight bold :height 2.0))))
+ '(org-level-2 ((t (:inherit outline-1 :weight bold :height 1.5))))
+ '(org-level-3 ((t (:inherit outline-1 :weight bold :height 1.2))))
+ '(variable-pitch ((t (:family "DejaVu Sans")))))
