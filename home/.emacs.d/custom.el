d79b23bf228ff5b3ae2a8990e921c9f93e3a348c
Stefanos Carslström <stefanos.carlstrom@gmail.com>
2014-09-18 22:27:02 +0200

    Misc package additions, eww support (Emacs 24.4)

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -6,7 +6,8 @@
  '(custom-safe-themes
    (quote
     ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
- '(doc-view-resolution 200))
+ '(doc-view-resolution 200)
+ '(eww-search-prefix "https://www.google.com/search?q="))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
