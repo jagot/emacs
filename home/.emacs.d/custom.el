7a65770a98eb4af0b26e307a7af87833cba91655
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2015-06-26 11:49:59 +0200

    Added Org LaTeX preview for source code files

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -7,7 +7,11 @@
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
  '(doc-view-resolution 200)
- '(eww-search-prefix "https://www.google.com/search?q="))
+ '(eww-search-prefix "https://www.google.com/search?q=")
+ '(org-format-latex-options
+   (quote
+    (:foreground default :background default :scale 2 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+                 ("begin" "$1" "$" "$$" "\\(" "\\[")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
