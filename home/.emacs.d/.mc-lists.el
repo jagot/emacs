ffef1b724bc78213ce218c27875794880938eff0
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2015-06-27 13:11:36 +0200

    Misc improvements, some Mac related

--- home/.emacs.d/.mc-lists.el
+++ home/.emacs.d/.mc-lists.el
@@ -9,6 +9,7 @@
         TeX-insert-dollar
         TeX-insert-sub-or-superscript
         backward-sexp
+        beginning-of-defun
         c-electric-brace
         c-electric-colon
         c-electric-delete-forward
@@ -18,6 +19,8 @@
         c-electric-slash
         c-electric-star
         comment-region
+        ediff-mark-for-hiding-at-pos
+        f90-electric-insert
         forward-sexp
         insert-char
         julia-latexsub-or-indent
@@ -27,21 +30,30 @@
         org-delete-char
         org-end-of-line
         org-export-dispatch
+        org-force-self-insert
         org-preview-latex-fragment
         org-return-indent
         org-self-insert-command
         org-yank
+        orgtbl-self-insert-command
         outshine-self-insert-command
         reindent-then-newline-and-indent
+        sh-assignment
         sp--self-insert-command
         sp-add-to-previous-sexp
         sp-wrap-cancel
         wdired-capitalize-word
+        wdired-downcase-word
+        wdired-next-line
+        wdired-previous-line
         yas-expand
         ))
 
 (setq mc/cmds-to-run-once
       '(
         backward-sentence
+        mac-mwheel-scroll
+        org-export-dispatch
         org-mouse-down-mouse
+        sp-remove-active-pair-overlay
         ))
