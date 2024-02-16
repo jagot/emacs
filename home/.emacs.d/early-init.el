37596d640a8b45cfb2a5ecf13005d09ecda78769
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2024-02-16 13:48:18 +0100

    Native compile flags

--- home/.emacs.d/early-init.el
+++ home/.emacs.d/early-init.el
@@ -1,4 +1,5 @@
 (setq package-enable-at-startup nil)
+(setq native-comp-async-report-warnings-errors nil)
 
 (let ((early-init-site-el (concat user-emacs-directory "early-init-site.el")))
   (if (file-exists-p early-init-site-el)
